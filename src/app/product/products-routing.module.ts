9f1d8223400e03fbb4d04d7232cab691cd6f487a
jeraldtulibao <jeraldtulibao@gmail.com>
2024-05-05 19:50:42 +0800

    first commit

---
+++ src/app/product/products-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsLevelThreeOneComponent } from './products-level-three-one/products-level-three-one.component';
+import { ProductsLevelThreeTwoComponent } from './products-level-three-two/products-level-three-two.component';
+import { ProductComponent } from './product.component';
+
+const routes: Routes = [
+ {  path:'level3.1',
+component:ProductsLevelThreeOneComponent
+},
+{  path:'level3.2',
+component:ProductsLevelThreeTwoComponent
+},
+{  path:'level1.2',
+component:ProductComponent
+}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProductsRoutingModule { }
